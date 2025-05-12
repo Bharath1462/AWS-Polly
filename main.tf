93acb2c38eb00884fd42c6788ec3e59760de4504
Bharath1462 <124774580+Bharath1462@users.noreply.github.com>
2025-05-12 21:15:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 provider "aws" {
-  region = "us-east-1"  # Set your AWS region here
+  region = var.region
 }
 
 module "s3" {
   source         = "./modules/s3"
-  input_bucket   = "my-polly-input-bucket"
-  output_bucket  = "my-polly-output-bucket"
+  input_bucket   = var.input_bucket
+  output_bucket  = var.output_bucket
 }
 
 module "iam" {
-  source         = "./modules/iam"
-  input_bucket_arn  =  module.s3.input_bucket_arn
-  output_bucket_arn =  module.s3.output_bucket_arn
+  source             = "./modules/iam"
+  input_bucket_arn   = module.s3.input_bucket_arn
+  output_bucket_arn  = module.s3.output_bucket_arn
 }
 
 module "lambda" {
-  source              = "./modules/lambda"
-  lambda_function_name = "PollyTextToSpeech"
-  role_arn            = module.iam.lambda_role_arn
-  output_bucket       = module.s3.output_bucket_name
-  input_bucket        = module.s3.input_bucket_name
+  source               = "./modules/lambda"
+  lambda_function_name = var.lambda_function_name
+  role_arn             = module.iam.lambda_role_arn
+  output_bucket        = module.s3.output_bucket_name
+  input_bucket         = module.s3.input_bucket_name
 }
